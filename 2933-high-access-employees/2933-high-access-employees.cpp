8edfc9ff81abdcaaa60473cff075160a6ada5b7f
Ajay yadav <94502917+Ajayyadav3@users.noreply.github.com>
2024-01-02 03:58:18 +0530

    Time: 96 ms (9.08%), Space: 44.4 MB (73.32%) - LeetHub

---
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+        // name -> access times
+        unordered_map<string, vector<int>> mp;
+
+        for (const auto& it : access_times) {
+            int time = stoi(it[1]); 
+            mp[it[0]].push_back(time);  
+        }
+
+        for (auto& it : mp)
+            sort(it.second.begin(), it.second.end());
+
+   
+     vector<string> ans;
+
+        for (const auto& it : mp) {
+            const vector<int>& time = it.second;
+            for (int i = 2; i < time.size(); ++i) {
+                if (time[i] - time[i - 2] < 100) {
+                    ans.push_back(it.first);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
