694b786c8aee08e7bf7b34fb6a113c61e8e086c8
Ajay yadav <94502917+Ajayyadav3@users.noreply.github.com>
2024-01-03 11:33:36 +0530

    Time: 1693 ms (37.24%), Space: 165.4 MB (86.91%) - LeetHub

---
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    vector<int> smallestTrimmedNumbers(vector<string>& numbers, vector<vector<int>>& queries) {
+        int numOfNumbers = numbers.size(); 
+        vector<pair<string, int>> trimmedNumbers(numOfNumbers); 
+        vector<int> answer;
+
+        
+        for (auto& query : queries) {
+            int k = query[0]; 
+            int trimLength = query[1]; 
+
+            
+            for (int i = 0; i < numOfNumbers; ++i) {
+                
+                trimmedNumbers[i] = {numbers[i].substr(numbers[i].size() - trimLength), i};
+            }
+
+            
+            sort(trimmedNumbers.begin(), trimmedNumbers.end());
+
+        
+            answer.push_back(trimmedNumbers[k - 1].second);
+        }
+
+        return answer;
+    }
+};
